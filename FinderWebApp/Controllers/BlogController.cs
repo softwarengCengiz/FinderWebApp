49604f01ddc4a29e53f9e00ac56c18e36804638e
Elif Cancı <elifcanci5@gmail.com>
2024-04-08 15:08:43 +0300

    blog sayfası oluşturuldu

---
+++ FinderWebApp/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+using FinderWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FinderWebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
