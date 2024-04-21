4206ebeb57ec5582e8dcc44b81d5869d5a5f1e80
cengizemre <cengi@DESKTOP-2FI0KH2>
2024-04-21 19:41:47 +0300

    Profil sayfası yapıldı

---
+++ FinderWebApp/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinderWebApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IHttpContextAccessor HttpContextAccessor;
+
+        public ProfileController(IHttpContextAccessor HttpContextAccessor)
+        {
+            this.HttpContextAccessor = HttpContextAccessor;
+        }
+        public IActionResult Index(Guid? id)
+        {
+            var role = HttpContextAccessor.HttpContext?.Request.Cookies["Role"];
+
+            if (role == "0")
+            {
+                // burada öğrenci servise istek at
+            } 
+            return View();
+        }
+    }
+}
